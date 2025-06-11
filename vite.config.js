a506336c14abb8cfb840782198faef6dc440f5f3
J1MMM <jimuelbaraero00@gmail.com>
2025-06-11 12:16:40 +0800

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import pagesPlugin from "vite-plugin-pages";
+import generateSitemap from "vite-plugin-pages-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    pagesPlugin({
+      onRoutesGenerated: (routes) => generateSitemap({ routes }),
+    }),
+  ],
+});
